2574bf1ac01518cde364553a64dca1cdd7d058c9
Chotharthana Monisha (EXT) <monisha.chotharthana@kone.com>
2025-03-25 15:09:30 +0530

    Modified AnonymousFunction.jsx

--- src/AnonymousFunction.jsx
+++ src/AnonymousFunction.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AnonymousFunction.css'
 
 const AnonymousFunction = () => {
     const handleName = () =>{
@@ -10,11 +11,17 @@ const AnonymousFunction = () => {
     const handleNameClick = (name) => {
         console.log(`Thanks ${name}`)
     }
+    const handleClick = (e) =>{
+        console.log(e)
+        console.log(e.target)
+        console.log(e.target.innerText)
+    }
   return (
     <div>
         <button onClick={handleName}>CLICK LOAD</button>
         <button onClick={handleNameTwo()}>AUTO LOAD</button>
         <button onClick={()=>handleNameClick("jenny")}>CLICK LOAD WITH PARAMETER</button>
+        <button onClick={handleClick}>GET EVENT</button>
     </div>
   )
 }
